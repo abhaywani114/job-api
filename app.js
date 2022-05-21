7ea8e0d3478dd804dd414b6cd3716dec5614d1d9
Abrar Ajaz <abhaywani114@gmail.com>
2022-05-21 16:16:42 +0530

    swagger DOCS

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const cors = require("cors");
 const helmet = require("helmet");
 const xss_clean = require("xss-clean");
 const rateLimit = require("express-rate-limit");
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+//middle ware exec
 app.use(cors());
 app.use(helmet());
 app.use(xss_clean());
@@ -23,7 +30,11 @@ app.use(rateLimit({
 }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Jobs API is up"));
+//basic requests
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //custom routes
 const authRoutes = require("./routes/auth")
